abd41316b4d7a8252ff9f9b5a6d29c98e2c9a28c
Vladyslav Mishchuk <v.mishuk2017@gmail.com>
2024-01-21 20:37:35 +0000

    I renaming let

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,10 @@
 `use strict`;
 function getElementWidth(content, padding, border) {
-  let contentWidth =
+  let elementWidth =
     Number.parseInt(content) +
     Number.parseInt(padding) * 2 +
     Number.parseInt(border) * 2;
-  return contentWidth;
+  return elementWidth;
 }
 console.log('task-3');
 console.log(getElementWidth('50px', '8px', '4px'));
